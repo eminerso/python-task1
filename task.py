ba2f883abdffb947f0142c9a12dc699b70591780
Emın <62133817+eminerso@users.noreply.github.com>
2024-05-02 21:31:10 +0300

    first commit

---
+++ task.py
@@ -0,0 +1,60 @@
+
+# listof=[]
+# num1=int(input("Enter a number:"))
+# listof.append(num1)
+# num2=int(input("Enter a number:"))
+# listof.append(num2)
+# num3=int(input("Enter a number:"))
+# listof.append(num3)
+# num4=int(input("Enter a number:"))
+# listof.append(num4)
+# num5=int(input("Enter a number:"))
+# listof.append(num5)
+
+# newlist=[]
+# while len(listof)>0:
+#     for x in listof:
+#         if x==min(listof):
+#             newlist.append(min(listof))
+#             listof.remove(min(listof))    
+# print(newlist)
+
+# word=input("Enter a string:")
+# preword=[]
+# neword=[]
+# for x in word:
+#     preword.append(x)
+
+# while len(preword)>0:
+#     for x in preword:
+#         if x==min(preword):
+#             neword.append(min(preword))
+#             preword.remove(min(preword))    
+# print(neword)
+
+# count = 1
+
+# getnum=int(input("Enter a number:"))
+# while not getnum==13:
+#     count+=1
+#     getnum=int(input("Enter a number:"))
+
+# print(f" Tebrikler {getnum} reqemini {count} cehdde tapdiniz")
+
+# for x in range(1,100):
+#     if x %2==0:
+#         print(x)
+
+
+
+
+
+
+
+    
+    
+
+
+
+            
+   
